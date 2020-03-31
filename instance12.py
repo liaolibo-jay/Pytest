6c12145f6e45e445b03730b7a83afd621070baf7
liaolibo-jay <liaolibo-jay@outlook.com>
2020-03-31 15:01:00 +0800

    modified the code of calculation

--- instance12.py
+++ instance12.py
@@ -12,30 +12,34 @@ for i in areport:               #把多行换成列表
     report.append(i)
 report[0] = report[0]+['总分','平均分']
 report[0].insert(0,'名次')
-report.insert(1,['1','平均'])
+report.insert(1,['平均'])
 totcs=[0,0,0,0,0,0,0,0,0]       #每门科目总分
 
-for scores in report[2:]:
-    tots = 0
-    for i in scores[1:]:
-        totcs[int(scores.index(i))-1]+=int(i)
-        tots +=int(i)
-    scores.append(str(tots))   #插入个人总分
-    scores.append(str(format((tots/9),'.2f'))) #插入个人平均分
+for i in range(1,len(report[2])):
+    for j in range(2,len(report)):
+        totcs[i-1] += int(report[j][i])
 
 for i in totcs:                 #改成平均分
     x = format(int(i)/30,'.2f')
     totcs[totcs.index(i)]=str(x)
 
-report[1] = report[1]+totcs     #插入科目总分
+report[1] = report[1]+totcs     #插入科目均分
+
+for scores in report[1:]:
+    tots = 0
+    for i in scores[1:]:
+        tots +=float(i)
+    scores.append(str(tots))   #插入个人总分
+    scores.append(str(format((tots/9),'.2f'))) #插入个人平均分
+
 for i in report:
     i.append('\n')
 
 report[2:]=sorted(report[2:],key=lambda x:x[-2],reverse=True) #按平均分排序（除开头两行）
-for i in range(2,32):
-    report[i].insert(0,'%i' %(i))
+for i in range(1,32):
+    report[i].insert(0,'%i' %(i-1))
 
-for i in range(3,33):
+for i in range(2,33):
     for j in range(2,13):
         try:
             if float(report[i][j]) < 60.0:
