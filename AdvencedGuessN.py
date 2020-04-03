d6a6e3e973b680d776117a4954bb9938e54ebcb5
liaolibo-jay <liaolibo-jay@outlook.com>
2020-04-03 13:20:07 +0800

    2020.04.03

--- AdvencedGuessN.py
+++ AdvencedGuessN.py
@@ -2,26 +2,26 @@ import os
 import requests
 
 accountment=[]
-with open('gUser.txt') as f:
+with open('gUser.txt') as f:  #打开文件后对数据做初步整理
     raccountment = f.readlines()
 for i in raccountment:
     i = i.strip().split()
     accountment.append(i)
 nindex =0
-name = input('请输入你的名字：')
+name = input('请输入你的名字：') #如果已经有名字，直接输出之前的结果以及记录该名字的次序
 for i in range(len(accountment)):
     if name == accountment[i][0]:
         print('%s你已经玩了%i次，最少%i轮猜出答案，平均%.2f轮猜出答案' %(accountment[i][0],int(accountment[i][1]),int(accountment[i][2]),float(int(accountment[i][3])/int(accountment[i][1]))))
         break
     else:
         nindex += 1
-if nindex == len(accountment):
+if nindex == len(accountment):  #遍历之后没有发现，则添加一个新名字
     accountment.append([name,'0','0','0'])
     print('%s你已经玩了%i次，最少%i轮猜出答案，平均%.2f轮猜出答案' %(name,0,0,0))
 
-r = requests.get('https://python666.cn/cls/number/guess/').text
+r = requests.get('https://python666.cn/cls/number/guess/').text #得到底数
 nRound = 1
-while True:
+while True:                                 #开始猜数字，至少有一轮，因此开始序数为1
     gNumber = input('请猜一个1-100的数字：')
     if gNumber.isdigit():
         if gNumber == r:
@@ -33,12 +33,12 @@ while True:
             print('猜小了，再试试：')
     nRound += 1
 
-accountment[nindex][1] = str(int(accountment[nindex][1])+1)
+accountment[nindex][1] = str(int(accountment[nindex][1])+1)  #修改为新结果
 accountment[nindex][3] = str(int(accountment[nindex][3])+nRound)
-if nRound < int(accountment[nindex][2]):
+if nRound < int(accountment[nindex][2]):                     #比较最快轮数并修改
     accountment[nindex][2] = str(nRound)
 
-for i in accountment:
+for i in accountment:                                           #整理文件并输出
     i.append("\n")
     accountment[accountment.index(i)] = ' '.join(i)
 with open('gUser.txt','w') as f:
